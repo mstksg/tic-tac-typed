8ba9589ab267f2bbe096c2ee8de184b41235f01b
Justin Le <justin@jle.im>
2018-10-26 19:22:59 -0700

    clean up sel

--- src/Data/Type/Sel.hs
+++ src/Data/Type/Sel.hs
@@ -15,22 +15,8 @@
 
 module Data.Type.Sel (
     Sel(..)
-  -- , mapIx, sMapIx, MapIx
-  -- , setIx, sSetIx, SetIx
-  -- , overSel
-  , setSel
   , listSel
   , OutOfBounds
-  -- -- * Index
-  -- , Index, SomeSel, pattern IZ, pattern IS
-  -- * Proofs
-  , mapIx_proof
-  , setIx_proof
-  -- * Defunctionalization Symbols
-  , MapIxSym0, MapIxSym1, MapIxSym2, MapIxSym3
-  , SetIxSym0, SetIxSym1, SetIxSym2, SetIxSym3
-  -- , IndexSym0, IndexSym1, IndexSym2
-  -- , SomeSelSym0, SomeSelSym1, SomeSelSym2, SomeSelSym3
   ) where
 
 import           Data.Kind
@@ -42,56 +28,12 @@ import           Data.Singletons.TH
 import           Data.Type.Nat
 
 -- TODO: implement Sel in terms of Index?
--- TODO: use lens-typelevel?
 
 -- | A @'Sel' n as a@ is an index into a list @as@ that the @n@th index is
 -- @a@.
 data Sel :: N -> [k] -> k -> Type where
     SelZ :: Sel 'Z (a ': as) a
     SelS :: Sel n as a -> Sel ('S n) (b ': as) a
-
-$(singletons [d|
-  mapIx :: N -> (a -> a) -> [a] -> [a]
-  mapIx Z     f (x:xs) = f x : xs
-  mapIx (S n) f (x:xs) =   x : mapIx n f xs
-
-  setIx :: N -> a -> [a] -> [a]
-  setIx i x = mapIx i (const x)
-  |])
-
-mapIx_proof
-    :: forall n as a f. ()
-    => Sel n as a
-    -> Sing as
-    -> Sel n (MapIx n f as) (f @@ a)
-mapIx_proof = \case
-    SelZ -> \case
-      _ `SCons` _  -> SelZ
-    SelS n -> \case
-      _ `SCons` xs -> SelS $ mapIx_proof @_ @_ @_ @f n xs
-
-setSel
-    :: forall k n (as :: [k]) (a :: k) (b :: k). ()
-    => Sel n as a
-    -> Sing b
-    -> Sing as
-    -> Sing (SetIx n b as)
-setSel = \case
-    SelZ -> \y -> \case
-      _ `SCons` xs -> y `SCons` xs
-    SelS n -> \y -> \case
-      x `SCons` xs -> x `SCons` setSel n y xs
-
-setIx_proof
-    :: forall n as a b. ()
-    => Sel n as a
-    -> Sing as
-    -> Sel n (SetIx n b as) b
-setIx_proof = \case
-    SelZ -> \case
-      _ `SCons` _ -> SelZ
-    SelS n -> \case
-      _ `SCons` xs -> SelS $ setIx_proof n xs
 
 type OutOfBounds n (as :: [k]) = Refuted (Σ k (TyCon (Sel n as)))
 
